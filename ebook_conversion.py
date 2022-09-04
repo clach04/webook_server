a0d4dc2acf1dc23e0f73cb08557785d13f11a2f7
clach04 <clach04@gmail.com>
2022-09-04 11:46:22 -0700

    Implement calling exe support for ebook conversion

--- ebook_conversion.py
+++ ebook_conversion.py
@@ -6,8 +6,14 @@
 All implementations rely on temp disk space and will spool to disk.
 """
 
+import logging
 import os
 import sys
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig()  # TODO include function name/line numbers in log
+log.setLevel(level=logging.DEBUG)  # Debug hack!
 
 
 """Boiler plate from /usr/bin/ebook-convert
@@ -22,19 +28,50 @@ sys.extensions_location = os.environ.get('CALIBRE_EXTENSIONS_PATH', '/usr/lib/ca
 sys.executables_location = os.environ.get('CALIBRE_EXECUTABLES_PATH', '/usr/bin')
 
 
-import calibre
-from calibre.ebooks.conversion.cli import main as calibre_ebook_convert
+try:
+    import calibre
+    from calibre.ebooks.conversion.cli import main as calibre_ebook_convert
+except ImportError:
+    calibre = None
+    import subprocess
+
 
 # Wrapper functions
+if calibre:
+    def convert_version():
+        return 'calibre_' + calibre.__version__
 
-def convert_version():
-    return 'calibre_' + calibre.__version__
+    def convert(original_filename, new_filename):
+        # This is not a fast operation, examples;
+        #                   700Kb azw3 can take almost 30 secs to convertion into mobi
+        # (same)    700Kb azw3 can take almost 10 secs to convertion into epub
+        # TODO capture stdout/stderr?
+        result = calibre_ebook_convert(['dummy', original_filename, new_filename])
+        return result  # or the new_filename?
+else:
+    # calibre external ebook-convert binary/exe/script
 
-def convert(original_filename, new_filename):
-    # This is not a fast operation, examples;
-    #                   700Kb azw3 can take almost 30 secs to convertion into mobi
-    # (same)    700Kb azw3 can take almost 10 secs to convertion into epub
-    # TODO capture stdout/stderr?
-    result = calibre_ebook_convert(['dummy', original_filename, new_filename])
-    return result  # or the new_filename?
+    calibre__version__ = '???'
+    ebook_convert_exe = os.environ.get('CALIBRE_EBOOK_CONVERT_EXE', 'ebook-convert')
+    # set CALIBRE_EBOOK_CONVERT_EXE=C:\Users\clach04\Calibre Portable\Calibre\ebook-convert.exe
+    # NOTE no double quotes, even though there are spaces in the path
+    log.debug('ebook_convert_exe %r', ebook_convert_exe)
 
+    process = subprocess.Popen([ebook_convert_exe, '--version'], stdout=subprocess.PIPE)  # call ebook-convert as a subprocess
+    process.wait()  # wait until it finishes it work
+    ebook_convert_exe_version_stdout, ebook_convert_exe_version_stderr = process.communicate()
+    log.debug('ebook_convert_exe_version_stderr %r', ebook_convert_exe_version_stderr)
+    log.debug('ebook_convert_exe_version_stdout %r', ebook_convert_exe_version_stdout)
+    ebook_convert_exe_version_stdout = ebook_convert_exe_version_stdout.decode('utf-8')
+    log.debug('ebook_convert_exe_version_stdout %r', ebook_convert_exe_version_stdout)
+    calibre__version__ = ebook_convert_exe_version_stdout.split(')', 1)[0].rsplit(' ', 1)[1]
+
+    def convert_version():
+        return 'calibre-ebook-convert_' + calibre__version__
+
+    def convert(original_filename, new_filename):
+        process = subprocess.Popen([ebook_convert_exe, original_filename, new_filename], stdout=subprocess.PIPE)  # call ebook-convert as a subprocess
+        process.wait()  # wait until it finishes it work
+        ebook_convert_exe_convert_stdout, ebook_convert_exe_convert_stderr = process.communicate()  # get output
+        log.debug('ebook_convert_exe_convert_stderr %r', ebook_convert_exe_convert_stderr)
+        log.debug('ebook_convert_exe_convert_stdout %r', ebook_convert_exe_convert_stdout)
